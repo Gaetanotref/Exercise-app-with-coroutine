898517c1153579259f19255ca93376b9b55642d3
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2023-02-17 16:09:57 +0100

    Commit init

---
+++ app/src/main/java/com/example/exercise_app_with_coroutine/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.exercise_app_with_coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.exercise_app_with_coroutine.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+class MainActivity : AppCompatActivity() {
+    private var currentValue = 0
+    private var inputValue: Int? = null
+    lateinit var mainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(mainBinding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mainBinding.button.setOnClickListener {
+            if (inputValue == null) {
+                inputValue = mainBinding.editTextNumber.text.toString().toIntOrNull()
+                currentValue = (inputValue?.plus(1)) ?: 0
+            } else {
+                currentValue++
+            }
+            runBlocking {
+                delay(2000)
+                mainBinding.textView.text = currentValue.toString()
+            }
+
+        }
+    }
+}
